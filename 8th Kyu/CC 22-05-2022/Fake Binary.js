3f4d71269412ce48c45a24e2778a6e29a582efab
shubhamsigdar <shubhamsigdar@gmail.com>
2022-05-24 17:55:33 +0530

    Adding Coding Challenge for 22-05-2022

--- 8th Kyu/CC 22-05-2022/Fake Binary.js
+++ 8th Kyu/CC 22-05-2022/Fake Binary.js
@@ -7,23 +7,29 @@
 //E:
 //P: 
 
-// function fakeBin(x){
-//     //convert string to number
-//     let y=parseInt(x);
-//     //convert number to array
-//     const array = Array.from(String(y), Number);
-//     let newArray=array.map(items=>{
-//       if(items<5){
-//         return 0;
-//       }
-//       else{
-//         return 1;
-//       }
-//     });
-//   let returnString=newArray.join('')
-//     return returnString;
+function fakeBin(x){
+    //convert string to number
+   
+    //convert number to array
+    const array = Array.from(String(x), Number);
+    let newArray=array.map(items=>{
+      if(items>=5){
+        return 1;
+      }
+      else{
+       return 0;
+      }
+    });
+  let returnString=newArray.join('')
+    return returnString;
     
-//   }
+  }
+  console.log(fakeBin('45385593107843568'), '01011110001100111')
+  console.log(fakeBin('509321967506747'), '101000111101101')
+  console.log(fakeBin('366058562030849490134388085'), '011011110000101010000011011')
+
+
+  
 function fakeBin(str){
     var newStr = "";
     for(var i=0;i<str.length;i++){
